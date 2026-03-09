5f678f853cd646c4cb558758cd32c68b2c7d4f9b
Denis M. Nzomo <denismuendo1611@gmail.com>
2026-03-09 12:51:13 +0300

    aded a comment of 3 F grades

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        int failCount = 0;
+
+        while (true)
+        {
+            Console.Write("Enter your grade percentage: ");
+            int percentage = int.Parse(Console.ReadLine());
+
+            string letter = "";
+            if (percentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (percentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (percentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (percentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            // Determine sign
+            int lastDigit = percentage % 10;
+            string sign = "";
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+
+            // Handle special cases
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+            if (letter == "F")
+            {
+                sign = "";
+            }
+
+            Console.WriteLine($"Your grade is {letter}{sign}");
+
+            if (percentage >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the course.");
+                break;
+            }
+            else
+            {
+                failCount++;
+                if (failCount == 3)
+                {
+                    Console.WriteLine("Keep trying! You'll get it next time.");
+                    break;
+                }
+            }
+        }
     }
 }
