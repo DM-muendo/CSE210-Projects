678ef41b880f611db69b38fb6149f68bcca00a8b
Denis M. Nzomo <denismuendo1611@gmail.com>
2026-03-09 21:28:17 +0300

    At the top of the file, you will see a comment that asks the player to enter his or her name:

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Request names at the start
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine($"Welcome to Guess My Number, {name}!");
+        Console.WriteLine();
+
+        string playAgain = "yes";
+        
+        // Main game loop - allows replaying
+        while (playAgain.ToLower() == "yes")
+        {
+            PlayGame();
+            
+            Console.WriteLine();
+            Console.Write("Would you like to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine();
+        }
+        
+        Console.WriteLine($"Thank you for playing, {name}! Goodbye!");
+    }
+    
+    static void PlayGame()
+    {
+        // Generate random number between 1 and 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        
+        int guess = 0;
+        int guessCount = 0;
+        
+        // Loop until user guesses the magic number
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+            
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        
+        // Display number of guesses
+        Console.WriteLine($"It took you {guessCount} guess(es) to get the answer!");
     }
 }
